1dadbf31fd074ccf926c630782fe3acbceb66a7e
Big_A <97909214+ArianAghamohseni@users.noreply.github.com>
2023-06-23 22:23:22 +0330

    Update ApiClient.java

--- API/ApiClient.java
+++ API/ApiClient.java
@@ -14,8 +14,8 @@ public class ApiClient {
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Content-Type", "application/json");
 
-            // Step 4: Create request body
-            String requestBody = "{\"name\":\"John Doe\",\"email\":\"johndoe@example.com\",\"password\":\"password123\"}";
+            // Step 4: Create request body for creating user account
+            String requestBody = "{\"username\":\"JohnDoe\",\"email\":\"johndoe@example.com\",\"password\":\"password123\"}";
 
             // Step 5: Write request body to request stream
             OutputStream outputStream = connection.getOutputStream();
@@ -36,6 +36,34 @@ public class ApiClient {
                 reader.close();
                 String jsonResponse = response.toString();
                 System.out.println(jsonResponse); // Success - display account created message and user data
+
+                // Get the user ID from the response JSON
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                int userId = jsonObject.getInt("id");
+
+                // Update the request URL to add the user ID to get liked songs
+                String likedSongsUrl = "http://localhost:3000/api/users/" + userId + "/liked_songs";
+                url = new URL(likedSongsUrl);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Content-Type", "application/json");
+
+                // Read response for getting liked songs
+                responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = connection.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+                    response = new StringBuffer();
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    reader.close();
+                    jsonResponse = response.toString();
+                    System.out.println(jsonResponse); // Display the user's liked songs
+                } else {
+                    // Handle error response
+                }
+
             } else {
                 // Handle error response
             }
